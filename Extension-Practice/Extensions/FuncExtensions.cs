1a485796fa586248b07e4c2ae8bec669c8c494bc
Eren YILMAZ <erenyilmazgazi@gmail.com>
2021-06-05 09:49:29 +0300

    init

---
+++ Extension-Practice/Extensions/FuncExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Extension_Practice.Extensions
+{
+    public static class FuncExtensions
+    {
+        public static bool ExecuteInTryCatch<TException>(this Func<object> function, ref Exception handledException) where TException : Exception
+        {
+
+            try
+            {
+                function();
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                handledException = ex;  //KULLANICIYA HATAYI DÖNDÜR
+
+                if (ex.GetType() == typeof(TException))
+                    return false;
+
+                //KULLANICININ BELİRTTİĞİ HATAYA TAKILMADI
+                return true;
+
+            }
+        }
+    }
+}
---
+++ Extension-Practice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Extension_Practice.Extensions;
+
+namespace Extension_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ExtensionTest();
+           Console.ReadLine();
+        }
+
+
+
+        static void ExtensionTest()
+        {
+            Exception handledException = null;
+
+            Func<object> function = () =>
+            {
+                Debug.Write("Extension Deniyoruz.");
+                throw new ArgumentNullException();
+            };
+
+
+            //DELEGEYİ TRY-CATCH İÇERİSİNDE ÇALIŞTIR.
+            //APPLICATION
+            var result = function.ExecuteInTryCatch<ApplicationException>(ref handledException);
+
+            if (result && handledException != null)
+            {
+                Debug.Write("Başka Bir Hata Oluştu.");
+            }
+        }
+    }
+}
